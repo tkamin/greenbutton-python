37e860eba158846f3814db5e10a8ade3c7c99b7a
Todd Kamin <toddkamin@hotmail.com>
2024-08-20 08:19:06 -0600

    Add parsing of XML string

--- greenbutton/parse.py
+++ greenbutton/parse.py
@@ -8,9 +8,15 @@ from datetime import timedelta
 
 from resources import *
 
+def parse_str(xml):
+    tree = ET.ElementTree(ET.fromstring(xml))
+    return extract_tree(tree)
+
 def parse_feed(filename):
     tree = ET.parse(filename)
+    return extract_tree(tree)
 
+def extract_tree(tree):
     usagePoints = []
     for entry in tree.getroot().findall('atom:entry/atom:content/espi:UsagePoint/../..', ns):
         up = UsagePoint(entry)
@@ -52,3 +58,19 @@ if __name__ == '__main__':
                 #if len(ir.readingQualities) > 0:
                 #    print('[%s]' % ', '.join([rq.quality.name for rq in ir.readingQualities]))
                 #print
+
+    with open(sys.argv[1], 'r') as file:
+        file_content = file.read()
+        print(len(file_content))
+        ups = parse_str(file_content)
+        for up in ups:
+            print('UsagePoint (%s) %s %s:' % (up.title, up.serviceCategory.name, up.status))
+            for mr in up.meterReadings:
+                print('  Meter Reading (%s) %s:' % (mr.title, mr.readingType.uom.name))
+                for ir in mr.intervalReadings:
+                    print('    %s, %s: %s %s' % (ir.timePeriod.start, ir.timePeriod.duration, ir.value, ir.value_symbol))
+                    #if ir.cost is not None:
+                    #    print(' (%s%s)' % (ir.cost_symbol, ir.cost))
+                    #if len(ir.readingQualities) > 0:
+                    #    print('[%s]' % ', '.join([rq.quality.name for rq in ir.readingQualities]))
+                    #print
